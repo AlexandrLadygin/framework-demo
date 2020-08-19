8f8444e211760a284dd5a4057195c91cb7e2e312
Alexandr Ladygin <ladygin04@gmail.com>
2020-08-19 20:55:41 +0300

    added properties, TestDataReader and refactored ComputeEngineCreator

--- framework-demo-project/src/test/java/service/ComputeEngineCreator.java
+++ framework-demo-project/src/test/java/service/ComputeEngineCreator.java
@@ -4,20 +4,28 @@ import model.ComputeEngine;
 
 public class ComputeEngineCreator {
 
-    public static final String NUMBER_OF_INSTANCE = "4";
-    public static final String WHAT_ARE_THESE_INSTANCES_FOR = "";
-    public static final String OPERATING_SYSTEM_SOFTWARE = "Free: Debian, CentOS, CoreOS, Ubuntu, or other User Provided OS";
-    public static final String MACHINE_CLASS = "Regular";
-    public static final String MACHINE_TYPE = "n1-standard-8 (vCPUs: 8, RAM: 30GB)";
-    public static final String NUMBER_OF_GPUS = "1";
-    public static final String GPU_TYPE = "NVIDIA Tesla V100";
-    public static final String LOCAL_SSD = "2x375 GB";
-    public static final String DATA_CENTER_LOCATION = "Frankfurt (europe-west3)";
-    public static final String COMMITTED_USAGE = "1 Year";
+    public static final String NUMBER_OF_INSTANCE = "testdata.computeengine.numberofinstance";
+    public static final String WHAT_ARE_THESE_INSTANCES_FOR = "testdata.computeengine.whataretheseinstancesfor";
+    public static final String OPERATING_SYSTEM_SOFTWARE = "testdata.computeengine.operatingsystemsoftware";
+    public static final String MACHINE_CLASS = "testdata.computeengine.machineclass";
+    public static final String MACHINE_TYPE = "testdata.computeengine.machinetype";
+    public static final String NUMBER_OF_GPUS = "testdata.computeengine.numberofgpus";
+    public static final String GPU_TYPE = "testdata.computeengine.gputype";
+    public static final String LOCAL_SSD = "testdata.computeengine.localssd";
+    public static final String DATA_CENTER_LOCATION = "testdata.computeengine.datacenterlocation";
+    public static final String COMMITTED_USAGE = "testdata.computeengine.committedusage";
 
     public static ComputeEngine withCredentialsProperties (){
-        return new ComputeEngine(NUMBER_OF_INSTANCE, WHAT_ARE_THESE_INSTANCES_FOR, OPERATING_SYSTEM_SOFTWARE, MACHINE_CLASS,
-                MACHINE_TYPE, NUMBER_OF_GPUS, GPU_TYPE, LOCAL_SSD, DATA_CENTER_LOCATION, COMMITTED_USAGE);
+        return new ComputeEngine(TestDataReader.getTestData(NUMBER_OF_INSTANCE),
+                TestDataReader.getTestData(WHAT_ARE_THESE_INSTANCES_FOR),
+                TestDataReader.getTestData(OPERATING_SYSTEM_SOFTWARE),
+                TestDataReader.getTestData(MACHINE_CLASS),
+                TestDataReader.getTestData(MACHINE_TYPE),
+                TestDataReader.getTestData(NUMBER_OF_GPUS),
+                TestDataReader.getTestData(GPU_TYPE),
+                TestDataReader.getTestData(LOCAL_SSD),
+                TestDataReader.getTestData(DATA_CENTER_LOCATION),
+                TestDataReader.getTestData(COMMITTED_USAGE));
     }
 
 

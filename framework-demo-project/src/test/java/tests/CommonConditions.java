948336182f2214317ef6dceb2dd6818a2e4a837e
Alexandr Ladygin <ladygin04@gmail.com>
2020-08-21 19:13:07 +0300

    added TestListener for creating screenshots when tests are failed

--- framework-demo-project/src/test/java/tests/CommonConditions.java
+++ framework-demo-project/src/test/java/tests/CommonConditions.java
@@ -4,8 +4,10 @@ import driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import utils.TestListener;
 
-
+@Listeners({TestListener.class})
 public abstract class CommonConditions {
 
     protected WebDriver driver;
